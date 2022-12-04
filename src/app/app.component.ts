99c285e4e829a6ae2127953a49e3068d788de2d2
Filip <filipbianga@pop-os.localdomain>
2022-12-04 13:40:28 +0100

    My first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ZadaniaRekrutFront';
+export class AppComponent implements OnInit{
+
+  session: any;
+   dataset: Numbers = {
+     numbers: [],
+     order: ''
+   };
+
+   constructor(private http: HttpClient) {
+   }
+
+   onSubmit() {
+     this.http.post<Numbers>('http://localhost:8080/numbers/sort-command', this.dataset, {responseType: 'text' as 'json'}).subscribe(
+       res => {
+         this.dataset = res;
+
+         localStorage.setItem('session', JSON.stringify(res))
+         window.location.reload();
+       }
+     )
+   }
+
+   get local(): any {
+     return localStorage.getItem('session');
+   }
+
+  ngOnInit(): void{
+  }
+
+}
+
+interface Numbers {
+  numbers: Array<number>;
+  order: string;
 }
